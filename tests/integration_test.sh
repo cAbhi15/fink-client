7e227f9c7681e1e590be9877b73f2b524939d308
Abhishek Chauhan <chauhan.abhishek1729@gmail.com>
2019-07-30 18:53:47 +0530

    revert unset -e flag

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -13,9 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# This is a simple integration test
-
+set -e
 # start Kafka in docker container
 docker-compose -p integration_test -f docker-compose-kafka.yml up -d
 
