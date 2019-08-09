c30b0cb29488c2be6ba4c377e4b8c0d8bfaf93f7
Abhishek Chauhan <chauhan.abhishek1729@gmail.com>
2019-08-09 06:48:56 +0530

    dummy coverage test

---
+++ coverage_test.py
@@ -0,0 +1,16 @@
+# dummy program to test coverage
+import os
+import time
+
+def main():
+    print("start of dummy program")
+
+    for i in range(3):
+        # do nothing
+        time.sleep(1)
+
+    print("end of dummy program")
+
+
+if __name__ == "__main__":
+    main()
